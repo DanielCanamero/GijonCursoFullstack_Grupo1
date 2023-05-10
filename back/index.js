fc5677edebfcb8b830543e000bf72e533e49bcb9
Daniel Cañamero Muñiz <danielcanamero97@gmail.com>
2023-05-10 03:46:47 +0200

    ultima actualizacion registro
    Ultima prueba del registro, conclusión: fallo del servidor error 500, no encuentro explicación alguna.

--- back/index.js
+++ back/index.js
@@ -18,15 +18,16 @@ const usuario = [{
     clave: "password"
 }];
 
-app.post("/login", function(request, response){
-    if(request.body.name == usuario.nombre || request.body.clave == usuario.clave){
-        var token = jwt.sign({ usuario: "usuario"}, jwtClave);
+// app.post("/login", function(request, response){
+//     if(request.body.name == usuario.nombre || request.body.clave == usuario.clave){
+//         var token = jwt.sign({ usuario: "usuario"}, jwtClave);
 
-        response.send(token);
-    }else{
-        response.status(401).end('Usuario incorrecto');
-    }
-})
+//         response.send(token);
+//     }else{
+//         response.status(401).end('Usuario incorrecto');
+//     }
+// })
+
 //funcional?
 app.post('/registro', async function (req, res) {
   try {
@@ -53,13 +54,13 @@ app.post('/registro', async function (req, res) {
 });
 
 
-async function findAllRows() {
-  sequelize
-    .query("SELECT * FROM users", { type: sequelize.QueryTypes.SELECT })
-    .then(function (users) {
-      console.log(users);
-    });
-}
+// async function findAllRows() {
+//   sequelize
+//     .query("SELECT * FROM users", { type: sequelize.QueryTypes.SELECT })
+//     .then(function (users) {
+//       console.log(users);
+//     });
+// }
 
 app.get("/users", async function (req, res) {
   try {
@@ -73,9 +74,9 @@ app.get("/users", async function (req, res) {
   }
 });
 
-app.get("/", function (req, res) {
-  res.sendFile(__dirname + "/public/index.html");
-});
+// app.get("/", function (req, res) {
+//   res.sendFile(__dirname + "/public/index.html");
+// });
 
 app.listen(3000, function () {
   console.log("Sistema funcionando en el puerto 3000");
